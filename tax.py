6cf7350c079d7ce06dec378d0ea801ee5e1ac37d
Kaitlin Green <kaitlin.green@stu.ocsb.ca>
2022-10-05 05:12:47 +0000

    I am so tired. here is the code to calculate tax and total with tax in Nunavut.

---
+++ tax.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# Created by: Katie G
+# Created on: October 3rd, 2022
+# This program gets the subtotal from
+# the user and then calculates the
+# amount of dollars the user will
+# pay in taxes if they make their purchase
+# in Nunavut using the tax rate of 5%
+# then the program calculates the amount
+# the user will pay in total,
+# subtotal and tax together.
+import tax_constants
+
+
+# this function calculates the dollars in tax
+# and then the total cost including tax
+# if the purchase is made in Nunavut.
+def main():
+    # getting the subtotal from the user.
+    subtotal = float(input("Please enter the subtotal in CAD: "))
+
+    # calculating the dollars spent in tax
+    # and the total spent.
+    taxDollars = subtotal * tax_constants.TAX_RATE
+    total = subtotal + taxDollars
+
+    # displaying the results of the above calculations
+    # back to the user.
+    print(
+        "In Nunavut, the amount in tax added"
+        " to your subtotal will be: ${:,.2f} CAD".format(taxDollars)
+        )
+    print(
+        "In Nunavut, with that subtotal, you will end up"
+        " paying ${:,.2f} CAD in total with taxes added.".format(total)
+        )
+
+
+if __name__ == "__main__":
+    main()
---
+++ tax_constants.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Created by: Katie G
+# Created on: October 3rd, 2022
+# this program contains the constants
+# for the tax program for Nunavut.
+
+TAX_RATE = 0.05
